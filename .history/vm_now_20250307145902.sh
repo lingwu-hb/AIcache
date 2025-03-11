8910db9b0ae1d109a228d0bb469b302dd55d4844
lzq <1021578619@qq.com>
2025-03-11 12:04:59 +0800

    new

--- .history/vm_now_20250307145902.sh
+++ .history/vm_now_20250307145902.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-# 手动测试
+# 手动测试 
 # 该脚本会启动SPDK和QEMU
 
 # 接管设备
 ${SPDK_PATH}/scripts/setup.sh
 
 # 分配大页
-echo 15000 >/sys/devices/system/node/node0/hugepages/hugepages-2048kB/nr_hugepages
-echo 5000 >/sys/devices/system/node/node1/hugepages/hugepages-2048kB/nr_hugepages
-echo 5000 >/sys/devices/system/node/node2/hugepages/hugepages-2048kB/nr_hugepages
-echo 5000 >/sys/devices/system/node/node3/hugepages/hugepages-2048kB/nr_hugepages
+echo 15000 > /sys/devices/system/node/node0/hugepages/hugepages-2048kB/nr_hugepages
+echo 5000 > /sys/devices/system/node/node1/hugepages/hugepages-2048kB/nr_hugepages
+echo 5000 > /sys/devices/system/node/node2/hugepages/hugepages-2048kB/nr_hugepages
+echo 5000 > /sys/devices/system/node/node3/hugepages/hugepages-2048kB/nr_hugepages
 
 sleep 1
 
@@ -21,7 +21,6 @@ cd ${SPDK_PATH}
 ./scripts/rpc.py bdev_nvme_attach_controller -b nvme0 -t PCIe -a 0000:83:00.0
 # ./scripts/rpc.py bdev_split_create -s 167 nvme0n1 1
 ./scripts/rpc.py bdev_split_create -s 8200 nvme0n1 1
-# TODO 修改缓存大小
 ./scripts/rpc.py bdev_rbd_create -b core1 vmdisk vm01 512
 ./scripts/rpc.py bdev_ocf_create CAS1 wt nvme0n1p0 core1 --cache-line-size 4
 scripts/rpc.py nvmf_create_transport -t VFIOUSER
@@ -30,17 +29,18 @@ scripts/rpc.py nvmf_subsystem_add_ns nqn.2021-06.io.spdk:ctc_device1 CAS1
 rm -rf /var/run/bar0 /var/run/cntrl
 scripts/rpc.py nvmf_subsystem_add_listener nqn.2021-06.io.spdk:ctc_device1 -t VFIOUSER -a /var/run -s 0
 
+
 #!/bin/bash
 #  手动连入虚拟机（自动修改xml文件，并且退出时修改回来）
 
-XML_FILE="/home/lzq/opencas_vm/vm01.xml" # 调整XML文件的目录
+XML_FILE="/home/lzq/opencas_vm/vm01.xml"  # 调整XML文件的目录
 
 sleep 1
-#
+# 
 virsh destroy vm01
 
 # 取消定义
-virsh undefine vm01 --nvram
+virsh undefine vm01 --nvram 
 
 sed -i 's#/var/run/vm01/cntrl#/var/run/cntrl#' "$XML_FILE"
 
@@ -54,3 +54,4 @@ virsh start vm01
 sed -i 's#/var/run/cntrl#/var/run/vm01/cntrl#' "$XML_FILE"
 
 exit
+
