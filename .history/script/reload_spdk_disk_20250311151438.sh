776a60ddf3d129fe8510e275401c5363303f1af7
lzq <1021578619@qq.com>
2025-03-11 15:18:11 +0800

    new

--- .history/script/reload_spdk_disk_20250311151438.sh
+++ .history/script/reload_spdk_disk_20250311151438.sh
@@ -25,46 +25,19 @@ while [[ $# -gt 0 ]]; do
     esac
 done
 
-# 1. 获取PCI设备信息
-PCI_ADDR="0000:83:00.0"
-PCI_DEV="pci_${PCI_ADDR//:/_}"
-
-# 2. 创建临时XML文件
-cat >/tmp/vfio.xml <<EOF
-<hostdev mode='subsystem' type='pci' managed='yes'>
-    <source>
-        <address domain='0x0000' bus='0x83' slot='0x00' function='0x0'/>
-    </source>
-</hostdev>
-EOF
-
-# 3. 分离设备
-virsh nodedev-detach $PCI_DEV
-
 cd ${SPDK_PATH}
 
-# 4. 删除现有SPDK配置
-./scripts/rpc.py nvmf_subsystem_remove_listener nqn.2021-06.io.spdk:ctc_device1 -t VFIOUSER -a /var/run -s 0
+# 1. 删除现有设备和配置
 ./scripts/rpc.py bdev_ocf_delete CAS1
-rm -rf /var/run/bar0 /var/run/cntrl
 
-# 5. 重新创建设备链
-./scripts/rpc.py bdev_nvme_attach_controller -b nvme0 -t PCIe -a ${PCI_ADDR}
+# 2. 重新创建设备链
+./scripts/rpc.py bdev_nvme_attach_controller -b nvme0 -t PCIe -a 0000:83:00.0
 ./scripts/rpc.py bdev_split_create -s ${CACHE_SIZE} nvme0n1 1
 ./scripts/rpc.py bdev_rbd_create -b core1 vmdisk vm01 512
 ./scripts/rpc.py bdev_ocf_create CAS1 wt nvme0n1p0 core1 --cache-line-size 4
 
-# 6. 重新配置VFIO传输
+# 3. 重新配置VFIO传输
 ./scripts/rpc.py nvmf_create_transport -t VFIOUSER
 ./scripts/rpc.py nvmf_create_subsystem nqn.2021-06.io.spdk:ctc_device1 -a -s sys1 -i 1 -I 32760
 ./scripts/rpc.py nvmf_subsystem_add_ns nqn.2021-06.io.spdk:ctc_device1 CAS1
 ./scripts/rpc.py nvmf_subsystem_add_listener nqn.2021-06.io.spdk:ctc_device1 -t VFIOUSER -a /var/run -s 0
-
-# 7. 重新附加设备到VM
-for vm_id in $(echo $VM_IDS | tr ',' ' '); do
-    VM_NAME="vm$(printf "%02d" $vm_id)"
-    virsh attach-device $VM_NAME /tmp/vfio.xml --current
-done
-
-# 8. 清理
-rm -f /tmp/vfio.xml
--- .history/script/reload_spdk_disk_20250311151459.sh
+++ .history/script/reload_spdk_disk_20250311151459.sh
@@ -25,46 +25,33 @@ while [[ $# -gt 0 ]]; do
     esac
 done
 
-# 1. 获取PCI设备信息
-PCI_ADDR="0000:83:00.0"
-PCI_DEV="pci_${PCI_ADDR//:/_}"
-
-# 2. 创建临时XML文件
-cat >/tmp/vfio.xml <<EOF
-<hostdev mode='subsystem' type='pci' managed='yes'>
-    <source>
-        <address domain='0x0000' bus='0x83' slot='0x00' function='0x0'/>
-    </source>
-</hostdev>
-EOF
-
-# 3. 分离设备
-virsh nodedev-detach $PCI_DEV
-
 cd ${SPDK_PATH}
 
-# 4. 删除现有SPDK配置
+# 1. 确保IOMMU已启用
+if ! grep -q "intel_iommu=on" /proc/cmdline; then
+    echo "Warning: IOMMU not enabled in kernel cmdline" >&2
+fi
+
+# 2. 解绑并重新绑定VFIO设备
+if [ -e /sys/bus/pci/devices/0000:83:00.0/driver ]; then
+    echo 0000:83:00.0 > /sys/bus/pci/devices/0000:83:00.0/driver/unbind
+fi
+echo "vfio-pci" > /sys/bus/pci/devices/0000:83:00.0/driver_override
+echo 0000:83:00.0 > /sys/bus/pci/drivers/vfio-pci/bind
+
+# 3. 删除现有设备和配置
 ./scripts/rpc.py nvmf_subsystem_remove_listener nqn.2021-06.io.spdk:ctc_device1 -t VFIOUSER -a /var/run -s 0
 ./scripts/rpc.py bdev_ocf_delete CAS1
 rm -rf /var/run/bar0 /var/run/cntrl
 
-# 5. 重新创建设备链
-./scripts/rpc.py bdev_nvme_attach_controller -b nvme0 -t PCIe -a ${PCI_ADDR}
+# 4. 重新创建设备链
+./scripts/rpc.py bdev_nvme_attach_controller -b nvme0 -t PCIe -a 0000:83:00.0
 ./scripts/rpc.py bdev_split_create -s ${CACHE_SIZE} nvme0n1 1
 ./scripts/rpc.py bdev_rbd_create -b core1 vmdisk vm01 512
 ./scripts/rpc.py bdev_ocf_create CAS1 wt nvme0n1p0 core1 --cache-line-size 4
 
-# 6. 重新配置VFIO传输
+# 5. 重新配置VFIO传输
 ./scripts/rpc.py nvmf_create_transport -t VFIOUSER
 ./scripts/rpc.py nvmf_create_subsystem nqn.2021-06.io.spdk:ctc_device1 -a -s sys1 -i 1 -I 32760
 ./scripts/rpc.py nvmf_subsystem_add_ns nqn.2021-06.io.spdk:ctc_device1 CAS1
 ./scripts/rpc.py nvmf_subsystem_add_listener nqn.2021-06.io.spdk:ctc_device1 -t VFIOUSER -a /var/run -s 0
-
-# 7. 重新附加设备到VM
-for vm_id in $(echo $VM_IDS | tr ',' ' '); do
-    VM_NAME="vm$(printf "%02d" $vm_id)"
-    virsh attach-device $VM_NAME /tmp/vfio.xml --current
-done
-
-# 8. 清理
-rm -f /tmp/vfio.xml
