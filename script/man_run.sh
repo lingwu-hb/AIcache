367004f4e724f5118aff375def39d6263fb41134
lzq <1021578619@qq.com>
2025-03-12 15:28:03 +0800

    new

--- script/man_run.sh
+++ script/man_run.sh
@@ -9,13 +9,11 @@ source ${SCRIPT_DIR}/config.sh
 # 测试trace配置
 replay_trace_config=(
     "ali-dev-5.txt 91"
-    # "hm_0.txt 96" # 每次跑HM都卡住！
     "mds_1.txt 4300"
     "prn_0.txt 193"
     "proj_0.txt 91"
     "proj_3.txt 270"
     # "prxy_0.txt 63"
-    # "rsrch_0.txt 17"
     # "rsrch_2.txt 68"
     # "src1_2.txt 81"
     # "src2_0.txt 41"
@@ -33,6 +31,8 @@ replay_trace_config=(
     # "proj_2.txt 20990"
     # "ali-dev-3.txt 8200"
 )
+# "hm_0.txt 96" # 每次跑HM都卡住！
+# "rsrch_0.txt 17"
 
 # 算法配置
 algo_config=(
