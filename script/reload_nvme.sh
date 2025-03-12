dee502ae5629139c143d5eb7bee033e930472f88
lzq <1021578619@qq.com>
2025-03-12 16:31:00 +0800

    new

--- script/reload_nvme.sh
+++ script/reload_nvme.sh
@@ -37,9 +37,7 @@ set -e # 添加错误检查
 for vm_id in $(echo $VM_IDS | tr ',' ' '); do
     if ! ssh root@${VM_BASE_IP}.${200+${vm_id}} "lsblk | grep -q nvme"; then
         echo -e "${ERROR} VM${vm_id} 无nvme设备"
-        exit 1
     fi
-    lsblk_check $vm_id
 done
 
 cd ${SPDK_PATH}
@@ -56,14 +54,12 @@ sleep 3
 # ./scripts/rpc.py bdev_nvme_attach_controller -b nvme0 -t PCIe -a ${PCI_ADDR}
 if ! ./scripts/rpc.py bdev_split_create -s ${CACHE_SIZE} nvme0n1 1; then
     echo -e "${ERROR} 创建split设备失败"
-    exit 1
 fi
 sleep 3
 
 # 4. 重新创建OCF设备
 if ! ./scripts/rpc.py bdev_ocf_create CAS1 wt nvme0n1p0 core1 --cache-line-size 4; then
     echo -e "${ERROR} 创建OCF设备失败"
-    exit 1
 fi
 sleep 3
 
@@ -72,7 +68,6 @@ sleep 3
 # ./scripts/rpc.py nvmf_create_subsystem nqn.2021-06.io.spdk:ctc_device1 -a -s sys1 -i 1 -I 32760
 if ! ./scripts/rpc.py nvmf_subsystem_add_ns nqn.2021-06.io.spdk:ctc_device1 CAS1; then
     echo -e "${ERROR} 添加namespace失败"
-    exit 1
 fi
 sleep 3
 # ./scripts/rpc.py nvmf_subsystem_add_listener nqn.2021-06.io.spdk:ctc_device1 -t VFIOUSER -a /var/run -s 0
