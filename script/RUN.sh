6ff21c366585e402da2ad194fec78c3a53b75b0c
lzq <51268398+LiuziqiOvO@users.noreply.github.com>
2025-03-26 15:31:08 +0800

    Update RUN.sh

--- script/RUN.sh
+++ script/RUN.sh
@@ -6,32 +6,23 @@
 SCRIPT_DIR=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
 source ${SCRIPT_DIR}/config.sh
 
-# 测试trace配置
 replay_trace_config=(
-    "ali-dev-5.txt 91"
-    # "hm_0.txt 96"
-    # "mds_1.txt 4300"
-    "prn_0.txt 193"
-    "proj_0.txt 91"
-    "proj_3.txt 270"
-    # "prxy_0.txt 63"
-    # "rsrch_0.txt 17"
-    # "rsrch_2.txt 68"
-    # "src1_2.txt 81"
-    # "src2_0.txt 41"
-    # "src2_1.txt 981"
-    # "src2_2.txt 1040"
-    # "stg_1.txt 4075"
-    # "ts_0.txt 51"
-    # "usr_0.txt 109"
-    # "wdev_0.txt 41"
-    # "web_0.txt 369"
-    # "web_1.txt 188"
-    # "web_3.txt 46"
-    # "prn_1.txt 3779"
-    # "web_2.txt 3440"
-    # "proj_2.txt 20990"
-    # "ali-dev-3.txt 8200"
+	"mds_1.txt 4300"
+	# "proj_0.txt 91"
+	# "proj_3.txt 270"
+	# "prxy_0.txt 63"
+	# "rsrch_2.txt 68"
+	# "src1_2.txt 81"
+	# "src2_1.txt 981"
+	"src2_2.txt 1040"
+	"stg_1.txt 4075"
+	# "ts_0.txt 51"
+	# "usr_0.txt 109"
+	# "web_0.txt 369"
+	# "web_1.txt 188"
+	# "prn_0.txt 193"
+	"ali-dev-5.txt 91"
+	"ali-dev-3.txt 8200"
 )
 
 # 算法配置(libdas.so)
