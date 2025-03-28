326aff4a960a68c0e1e06cc89c05c60f0b5b0b5f
lzq <51268398+LiuziqiOvO@users.noreply.github.com>
2025-03-28 14:50:53 +0800

    Update vm_now.sh

--- script/vm_now.sh
+++ script/vm_now.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# 手动测试
+# 手动测试 
 # 先手动开SPDK tgt  app
 # 该脚本会初始化SPDK和QEMU
 # Source configuration
@@ -9,27 +9,27 @@
 TOOL_DIR=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
 source ${TOOL_DIR}/config.sh
 
-XML_FILE="/home/lzq/AICache-tools/vm01.xml" # 调整XML文件的目录
+XML_FILE="/home/lzq/AICache-tools/vm01.xml"  # 调整XML文件的目录
 
 # 接管设备
 ${SPDK_PATH}/scripts/setup.sh
 
 # 分配大页
-echo 15000 >/sys/devices/system/node/node0/hugepages/hugepages-2048kB/nr_hugepages
-echo 5000 >/sys/devices/system/node/node1/hugepages/hugepages-2048kB/nr_hugepages
-echo 5000 >/sys/devices/system/node/node2/hugepages/hugepages-2048kB/nr_hugepages
-echo 5000 >/sys/devices/system/node/node3/hugepages/hugepages-2048kB/nr_hugepages
+echo 15000 > /sys/devices/system/node/node0/hugepages/hugepages-2048kB/nr_hugepages
+echo 5000 > /sys/devices/system/node/node1/hugepages/hugepages-2048kB/nr_hugepages
+echo 5000 > /sys/devices/system/node/node2/hugepages/hugepages-2048kB/nr_hugepages
+echo 5000 > /sys/devices/system/node/node3/hugepages/hugepages-2048kB/nr_hugepages
 
 sleep 1
 
 # ./scripts/setup.sh reset
 
 cd ${SPDK_PATH}
-./scripts/rpc.py log_set_level ERROR
-./scripts/rpc.py log_set_print_level ERROR
+./scripts/rpc.py log_set_level DEBUG
+./scripts/rpc.py log_set_print_level DEBUG
 ./scripts/rpc.py bdev_nvme_attach_controller -b nvme0 -t PCIe -a 0000:83:00.0
-# ./scripts/rpc.py bdev_split_create -s 167 nvme0n1 1
-./scripts/rpc.py bdev_split_create -s 8200 nvme0n1 1
+./scripts/rpc.py bdev_split_create -s 200 nvme0n1 1
+# ./scripts/rpc.py bdev_split_create -s 8200 nvme0n1 1
 ./scripts/rpc.py bdev_rbd_create -b core1 vmdisk vm01 512
 ./scripts/rpc.py bdev_ocf_create CAS1 wt nvme0n1p0 core1 --cache-line-size 4
 scripts/rpc.py nvmf_create_transport -t VFIOUSER
@@ -38,15 +38,17 @@ scripts/rpc.py nvmf_subsystem_add_ns nqn.2021-06.io.spdk:ctc_device1 CAS1
 rm -rf /var/run/bar0 /var/run/cntrl
 scripts/rpc.py nvmf_subsystem_add_listener nqn.2021-06.io.spdk:ctc_device1 -t VFIOUSER -a /var/run -s 0
 
+
 #!/bin/bash
 #  手动连入虚拟机（自动修改xml文件，并且退出时修改回来）
 
+
 sleep 1
-#
+# 
 virsh destroy vm01
 
 # 取消定义
-virsh undefine vm01 --nvram
+virsh undefine vm01 --nvram 
 
 sed -i 's#/var/run/vm01/cntrl#/var/run/cntrl#' "$XML_FILE"
 
