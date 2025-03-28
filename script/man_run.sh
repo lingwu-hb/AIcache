a203c0e5f6e3fc2166525a9d57f0452925d18caf
lzq <51268398+LiuziqiOvO@users.noreply.github.com>
2025-03-28 14:50:23 +0800

    Update man_run.sh

--- script/man_run.sh
+++ script/man_run.sh
@@ -8,42 +8,35 @@ source ${SCRIPT_DIR}/config.sh
 
 # 测试trace配置
 replay_trace_config=(
-    "ali-dev-5.txt 91"
-    "mds_1.txt 4300"
-    "prn_0.txt 193"
-    "proj_0.txt 91"
-    "proj_3.txt 270"
-    # "prxy_0.txt 63"
-    # "rsrch_2.txt 68"
-    # "src1_2.txt 81"
-    # "src2_0.txt 41"
-    # "src2_1.txt 981"
-    # "src2_2.txt 1040"
-    # "stg_1.txt 4075"
-    # "ts_0.txt 51"
-    # "usr_0.txt 109"
-    # "wdev_0.txt 41"
-    # "web_0.txt 369"
-    # "web_1.txt 188"
-    # "web_3.txt 46"
-    # "prn_1.txt 3779"
-    # "web_2.txt 3440"
-    # "proj_2.txt 20990"
-    # "ali-dev-3.txt 8200"
+	# "mds_1.txt 4300"
+	"proj_0.txt 91"
+	"proj_3.txt 270"
+	"prxy_0.txt 63"
+	"rsrch_2.txt 68"
+	"src1_2.txt 81"
+	"src2_1.txt 981"
+	"src2_2.txt 1040"
+	"stg_1.txt 4075"
+	# "ts_0.txt 51"
+	# "usr_0.txt 109"
+	# "web_0.txt 369"
+	# "web_1.txt 188"
+	# "prn_0.txt 193"
+	# "ali-dev-5.txt 91"
+	# "ali-dev-3.txt 8200"
 )
-# "hm_0.txt 96" #
-# "rsrch_0.txt 17"
 
 # SPDK 分支
 spdk_branch=(
     "baseline"
     "lzq"
     "hb"
+    "temp-merge"
 )
 # 算法配置
 algo_config=(
     "das-bind"
-    "no_prefetch"
+    # "no_prefetch"
 )
 
 # 虚拟机数量
